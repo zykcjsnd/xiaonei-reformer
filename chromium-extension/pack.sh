c9f1e067a133c0f17b7268981b1a7167fbca3de7
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2010-04-28 17:48:21 +0000

    .

--- chromium-extension/pack.sh
+++ chromium-extension/pack.sh
@@ -1,11 +1,4 @@
 #!/bin/sh -
 
-rm -rf ../pack ../pack.zip
-mkdir -p ../pack/image
-cp * ../pack
-cp image/* ../pack/image
-rm ../pack/pack.sh
-cd ../pack
-zip -r ../pack.zip *
-cd ..
-rm -rf pack
+rm -rf ../pack.zip
+zip -r ../pack.zip `find -type f | grep -Ev ".svn|pack.sh"`
