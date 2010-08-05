295133c38eb1e671ef7cb722f8b5874c9e7ad2d3
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2010-08-05 07:34:33 +0000

    tools

---
+++ tools/emoList.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/ruby
+require 'json'
+require 'net/http'
+require 'uri'
+
+url = URI.parse('http://status.renren.com/getdoingubblist.do')
+res = Net::HTTP.start(url.host, url.port) {|http|
+	http.get(url.path)
+}
+emoList = JSON.parse(res.body)["ubbList"]
+
+filename = File.dirname(__FILE__)+"/EMOTIONS"
+
+begin
+	file = File.open(filename,"r")
+	a = JSON.parse(file.gets())
+	file.close()
+rescue Errno::ENOENT
+	a = Hash.new()
+end
+
+emoList.each() {|emo|
+	a[emo["ubb"]]={"s" => emo["src"], "t" => emo["alt"]}
+}
+
+file = File.open(filename,"w")
+file.puts(JSON.generate(a))
+file.close()
