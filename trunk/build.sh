c3be1298ab6af9f8b89cf45e441bf9057b5a3697
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2010-08-23 13:36:21 +0000

    3.1.0.347

--- trunk/build.sh
+++ trunk/build.sh
@@ -7,6 +7,8 @@ VER1=`sed -n '9p' xiaonei_reformer.user.js | sed -E 's/ +/ /g' | cut -d' ' -f 3`
 VER2=`sed -n '10p' xiaonei_reformer.user.js | sed -E 's/ +/ /g' | cut -d' ' -f 3`
 VERSION=`echo $VER1 | sed -E 's/[0-9]+$/'$VER2'/'`
 
+sed -i "s/^XNR.version=\".*\";$/XNR.version=\"$VER1\";/;s/^XNR.miniver=.*;$/XNR.miniver=$VER2;/" $INPUT
+
 head -n 29 "$INPUT" > "$OUTPUT"
 yui-compressor --nomunge "$INPUT" >> "$OUTPUT"
 
