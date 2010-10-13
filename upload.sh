4cfda0a1475fcdfbe48b23e1114d1ea8b7946b4d
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2010-10-13 13:43:02 +0000

    3.2.0.364

--- upload.sh
+++ upload.sh
@@ -20,7 +20,7 @@ type1="-l Featured,OpSys-All,$VERSION"
 type2="-l OpSys-All,$VERSION"
 type3="-l Deprecated"
 
-./googlecode_upload.py -s "Greasemonkey脚本" $project $type0 xiaonei_reformer.min.user.js
+./googlecode_upload.py -s "Greasemonkey/Scriptish脚本" $project $type0 xiaonei_reformer.min.user.js
 ./googlecode_upload.py -s "Firefox扩展" $project $type1 xiaonei_reformer-fx.xpi
 ./googlecode_upload.py -s "脚本升级" $project $type3 45836.meta.js
 ./googlecode_upload.py -s "Chrome扩展" $project $type1 xiaonei_reformer.crx
