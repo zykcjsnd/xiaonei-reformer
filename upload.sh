91cb77b421064a709c5648b8e727bf600321029d
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2011-04-10 03:15:39 +0000

    418

--- upload.sh
+++ upload.sh
@@ -15,10 +15,9 @@ VER1=`sed -n ${VER1L}p xiaonei_reformer.user.js | sed -E 's/ +/ /g' | cut -d' ' 
 VER2L=`head -n 20 xiaonei_reformer.user.js | sed -n '/@miniver/='`
 VER2=`sed -n ${VER2L}p xiaonei_reformer.user.js | sed -E 's/ +/ /g' | cut -d' ' -f 3`
 
-VERSION=`echo $VER1 | sed -E 's/[0-9]+$/'$VER2'/'`
+VERSION="$VER1.$VER2"
 
 project="-p xiaonei-reformer -u xnreformer -w $passwd"
-type0="-l Featured,OpSys-All,$VER1,$VER2"
 type1="-l Featured,OpSys-All,$VERSION"
 type2="-l OpSys-All,$VERSION"
 type3="-l Deprecated"
@@ -28,7 +27,7 @@ type3="-l Deprecated"
 ./googlecode_upload.py -s "Safari升级" $project $type3 update.plist
 ./googlecode_upload.py -s "Chrome扩展" $project $type1 xiaonei_reformer.crx
 ./googlecode_upload.py -s "Firefox扩展" $project $type1 xiaonei_reformer-fx.xpi
-./googlecode_upload.py -s "Greasemonkey/Scriptish脚本" $project $type0 xiaonei_reformer.min.user.js
+./googlecode_upload.py -s "Greasemonkey/Scriptish脚本" $project $type1 xiaonei_reformer.min.user.js
 ./googlecode_upload.py -s "脚本升级" $project $type3 45836.meta.js
 ./googlecode_upload.py -s "发布日志" $project Release.txt
 
