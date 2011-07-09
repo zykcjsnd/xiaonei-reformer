e4c683f96054baf7181669b033a829d7d4d0bc16
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2011-07-09 09:23:43 +0000

    针对4.0+进行结构性能优化。不再支持3.6

--- trunk/firefox-extension/content/overlay.js
+++ trunk/firefox-extension/content/overlay.js
@@ -2,60 +2,32 @@
 
 Components.utils.import("resource://xiaonei-reformer/utils.js");
 
-var XNRChrome = {};
+var XNRChrome = {
+	obs: null,
 
-XNRChrome.eventListener = {
-	onLocationChange: function (aBrowser, aProgress, aRequest, aURI) {
-		if (aBrowser.contentDocument.readyState == "loading") {
-			if (XNRChrome.checkURL(aBrowser.contentWindow.location.href)) {
-				XNRChrome.injectScript(aBrowser.contentWindow);
-			}
+	injectScript: function (contentDocument) {
+
+		const whitelist="^https?://.*\\.renren\\.com/|^https?://renren\\.com/";
+		const blacklist="^http://wpi\\.renren\\.com/|ajaxproxy";
+
+		var url = contentDocument.baseURI;
+		if (!url || !url.match(whitelist) || url.match(blacklist)) {
+			return;
 		}
-	},
-	onProgressChange: function (aBrowser, aWebProgress, aRequest, curSelf, maxSelf, curTot, maxTot) {},
-	onStateChange: function (aBrowser, aWebProgress, aRequest, aFlag, aStatus) {},  
-	onStatusChange: function (aBrowser, aWebProgress, aRequest, aStatus, aMessage) {},
-	onSecurityChange: function (aBrowser, aWebProgress, aRequest, aState) {},
-	onRefreshAttempted: function (aBrowser, aWebProgress, aRefreshURI, aMillis,aSameURI) {
-		// must return true. or the refresh action will be blocked
-		return true;
-	},
-	onLinkIconAvailable: function (aBrowser) {}
-};
 
-XNRChrome.onframeLoaded = function (evt) {
-	var content = evt.target.defaultView.window;
-	if (content == content.top || !XNRChrome.checkURL(content.location.href)) {
-		return;
-	} else {
-		XNRChrome.injectScript(content);
+		var sandbox = XNRUtils.createSandbox(contentDocument.defaultView);
+	
+		XNRUtils.loadScript("chrome://xiaonei-reformer/content/xiaonei_reformer.user.js", sandbox);
 	}
 };
 
-XNRChrome.injectScript = function (contentWindow) {
-
-	var sandbox = XNRUtils.createSandbox(contentWindow);
-	
-	// can't use mozIJSSubScriptLoader due to https://bugzilla.mozilla.org/show_bug.cgi?id=377498
-	var script=XNRUtils.getUrlContents("chrome://xiaonei-reformer/content/xiaonei_reformer.user.js");
-
-	Components.utils.evalInSandbox(script, sandbox);
-};
-
-XNRChrome.checkURL = function (url) {
-	const whitelist="^https?://.*\\.renren\\.com/|^https?://renren\\.com/";
-	const blacklist="^http://wpi\\.renren\\.com/|ajaxproxy";
-	return url.match(whitelist) && !url.match(blacklist);
-};
-
 window.addEventListener("load", function () {
-	gBrowser.addTabsProgressListener(XNRChrome.eventListener);
-	gBrowser.addEventListener("DOMContentLoaded", XNRChrome.onframeLoaded, true);
+	XNRChrome.obs = XNRUtils.createObserver("document-element-inserted", XNRChrome.injectScript);
+	XNRChrome.obs.register();
 }, false);
 
 window.addEventListener("unload", function () {
-	gBrowser.removeEventListener("DOMContentLoaded", XNRChrome.onframeLoaded, true);
-	gBrowser.removeTabsProgressListener(XNRChrome.eventListener);
+	XNRChrome.obs.unregister();
 }, false);
 
 })();
