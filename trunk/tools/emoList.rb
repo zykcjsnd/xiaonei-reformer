f545be01416423cde76a0e0a092796f6bff004b7
xnreformer <xnreformer@69aaaa90-953b-11de-b51d-eb253e4b719b>
2012-03-15 03:14:30 +0000

    ...

--- trunk/tools/emoList.rb
+++ trunk/tools/emoList.rb
@@ -3,11 +3,27 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-def getEmo()
-	url = URI.parse('http://status.renren.com/getdoingubblist.do')
-	res = Net::HTTP.start(url.host, url.port) {|http|
-		http.get(url.path)
+def gfetch(url, cookie)
+	header = {
+		"Accept" => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+		"Referer" => 'http://www.renren.com/',
+		"Connection" => 'keep-alive',
+		"User-Agent" => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:10.0) Gecko/20100101 Firefox/11.0'
 	}
+	if (cookie != nil)
+		header["Cookie"] = cookie
+	end
+	uri = URI.parse(url)
+	http = Net::HTTP.new(uri.host, uri.port)
+	res = http.get(uri.path, header)
+end
+
+def getEmo()
+	cookie = IO.read(File.dirname(__FILE__)+"/cookies")
+	res = gfetch('http://status.renren.com/getdoingubblist.do', cookie)
+	if (res.code == '302')
+        raise 'Redirect to ' + res.reponse["location"]
+    end
 	return res.body
 end
 
@@ -19,8 +35,10 @@ rescue Errno::ETIMEDOUT
 	exit 1
 rescue SocketError
 	exit 1
-rescue JSON::ParserError
-	emoList = JSON.parse(getEmo())["ubbList"]
+rescue JSON::ParserError => e
+	p e.message
+	p e.backstack
+	exit 1
 end
 
 filename = File.dirname(__FILE__)+"/EMOTIONS"
